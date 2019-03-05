58fb77715be7dd8f467580fa31237272bdacd335
shanerego <35306988+shanerego@users.noreply.github.com>
2019-03-05 15:39:23 -0500

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,45 @@
+//Shane Rego 100623789 and Kizito Okoye 100611918
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import java.util.Random;
+
+public class Question1 extends Application {
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+
+        GridPane pane = new GridPane();
+        Random rand = new Random();
+
+        int img_no = rand.nextInt(54) + 1;
+        int img_no1 = rand.nextInt(54) + 1;
+        int img_no2 = rand.nextInt(54) + 1;
+
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(5);
+        pane.setVgap(5);
+
+        ImageView imageView1 = new ImageView(img_no+".png");
+        pane.add(imageView1, 0, 0);
+        ImageView imageView2 = new ImageView(img_no1+".png");
+        pane.add(imageView2, 1, 0);
+        ImageView imageView3 = new ImageView(img_no2+".png");
+        pane.add(imageView3, 2, 0);
+
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Question 1 Shane Rego"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
